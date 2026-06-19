5023a7779d6765a87ba630f4014cf764ad33f7ba
ImeshaDewmin <dewmin2001@gmail.com>
2026-06-19 19:52:52 +0530

    Remove unused Data Connect configuration and example files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,6 +8,6 @@ const API_KEY = process.env.REACT_APP_YT_API_KEY;
 
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=LK&key=${API_KEY}`;
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const OFFSET_LIVE_CHAT = 20;
